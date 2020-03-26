faaa1e38917da89b431027d8ad7399a11ceced6f
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-26 18:32:43 +0300

    Update: todoContext

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -3,8 +3,19 @@ import uuid from "uuid/v1";
 
 export const TodoContext = createContext();
 
-const TodoContextProvider = () => {
-  return <div></div>;
+const TodoContextProvider = props => {
+  const [todos, setTodos] = useState([
+    { title: "todo1", content: "ilk deneme icerigi", id: 1 },
+    { title: "todo2", content: "ikinci deneme icerigi", id: 2 }
+  ]);
+
+  const addTodo = (title, content) => {
+    setTodos([...todos, { title, content, id: uuid() }]);
+  };
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+  return <TodoContextProvider value={...todos, addTodo, removeTodo}>{props.childeren}</TodoContextProvider>;
 };
 
 export default TodoContextProvider;
