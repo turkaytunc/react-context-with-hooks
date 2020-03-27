cd7cee76142bc33e27729d86fabdb5cef2f53d98
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-27 13:37:01 +0300

    Update: list style

--- src/components/TodoDetails/TodoDetails.js
+++ src/components/TodoDetails/TodoDetails.js
@@ -6,8 +6,12 @@ const TodoDetails = ({ todo }) => {
   const { removeTodo } = useContext(TodoContext);
   return (
     <li onClick={() => removeTodo(todo.id)} className="todo-details-li">
-      <p className="todo-details-p todo-details-title">{todo.title}</p>
-      <p className="todo-details-p todo-details-content">{todo.content}</p>
+      <p className="todo-details-p todo-details-title">
+        {todo.title.charAt(0).toUpperCase() + todo.title.slice(1)}
+      </p>
+      <p className="todo-details-p todo-details-content">
+        {todo.content.charAt(0).toUpperCase() + todo.content.slice(1)}
+      </p>
     </li>
   );
 };
